192177e49992896bc8dbeea1db16424f03a6c648
JustRayCB <rayancbpro@gmail.com>
2024-02-25 18:52:29 +0100

    Api move to src/main.py but relative imports are still messy

--- src/main.py
+++ src/main.py
@@ -1,67 +1,60 @@
-"""Main module. Entry point of the application.
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-:Author: Rayan Contuliano Bravo
-:Date: 07/02/2024
-:Description: This module is the entry point of the application. It is responsible for starting the application and 
-loading the main window.
-"""
-
-from Analyse import BAnalysePath, OAnalysePath
+from Analyse import BAnalysePath
 from dijkstra import Dijkstra
-from Graph import BuildingGraph, OutsideGraph
+from Graph import BuildingGraph
 
-DATA_DIR = "data/plans/Solbosch/"
+DATA_DIR = "../data/plans/Solbosch/"
 BUILDINGS = ["P1"]
 
 
-def main():
-    """Main function. Entry point of the application."""
+app = FastAPI()
+origins = [
+    "http://localhost:63342",
+]
 
-    # graphs = {
-    #     building: BuildingGraph(f"{DATA_DIR}{building}/{building}.json") for building in BUILDINGS
-    # }
-    # while True:
-    #     # ask_building = input(f"Enter a building name, available buildings are {graphs.keys()}: ")
-    #     ask_building = "P1"
-    #     if ask_building == "exit":
-    #         return
-    #     assert ask_building in graphs, f"Building {ask_building} not found."
-    #     graph = graphs[ask_building]
-    #     while True:
-    #         # ask_start = input(f"Enter a start node, available nodes are {graph.nodes()}: ")
-    #         ask_start = "H1_1"
-    #         if ask_start == "exit":
-    #             return
-    #         assert ask_start in graph.nodes(), f"Node {ask_start} not found."
-    #         while True:
-    #             # ask_end = input(f"Enter an end node, available nodes are {graph.nodes()}: ")
-    #             ask_end = "E214_3"
-    #             if ask_end == "exit":
-    #                 return
-    #             assert ask_end in graph.nodes(), f"Node {ask_end} not found."
-    #             d = Dijkstra(graph, ask_start, ask_end)
-    #             our_path = d.path
-    #             networkx_path = graph.default_dijkstra(ask_start, ask_end)
-    #             assert our_path == networkx_path, "The paths are different."
-    #             d.show_shortest_path()
-    #             a = BAnalysePath(graph, our_path)
-    #             a.analyse()
-    #             print(a.get_instructions())
-    #             print(a.get_images())
-    #             return
-    graph = OutsideGraph(f"data/exits_positions/solbosch_map_updated.json")
-    position = (50.8344,4.368)
-    n = graph.find_closest_node(position)
-    print(n)
-
-    d = Dijkstra(graph, n, "Road 172")
-
-    o = OAnalysePath(graph, d.path)
-    print(d.path)
-    print(o.analyse())
-    d.show_shortest_path()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+graphs = {building: BuildingGraph(f"{DATA_DIR}{building}/{building}.json") for building in BUILDINGS}
 
 
-if __name__ == "__main__":
-    main()
-    
+class PathRequest(BaseModel):
+    start: str = None
+    arrival: str = None
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/api/available_buildings")
+def get_available_buildings():
+    return {"buildings": list(graphs.keys())}
+
+
+@app.post("/api/ask")
+def read_item(request: PathRequest, re: Request):
+    print("L'utilisateur veut aller de", request.start, "à", request.arrival)
+    start = request.start
+    arrival = request.arrival
+    # graph = graphs[start.split("_")[0]]
+    graph = graphs["P1"]
+    if start not in graph.nodes() or arrival not in graph.nodes():
+        return Response(status_code=404)
+    d = Dijkstra(graph, start, arrival)
+    a = BAnalysePath(graph, d.path)
+    a.analyse()
+    return {
+        "start": start,
+        "arrival": arrival,
+        "path": d.path,
+        "instructions": a.get_instructions(),
+    }
