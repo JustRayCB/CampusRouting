56b9e1e3201ba716ab7094b6906ae54c3fc5e541
zizou2k <zhaltout@gmail.com>
2024-03-03 19:18:20 +0100

    Moved the functions and now submit button works just need to plug the api functionalities ot it and it should be good to go

--- webpage/src/localisation.js
+++ webpage/src/localisation.js
@@ -16,23 +16,49 @@ if (navigator.geolocation) {
             [50.8145, 4.3823]
         ]);
 
+        var testing_latlngs = [
+            // [4.2301, 50.4849] // testing if coordinates are inversed or not
+            [50.8136, 4.3836], // 144
+            [50.8133, 4.3830], // 98
+            [50.8130555556, 4.3825], // 58
+            [50.8130555556, 4.3819], // 57
+            [50.8128, 4.3817], // 56
+            [50.8128, 4.3808], // 75
+            [50.8131, 4.3808], // 54
+            [50.8133, 4.3808], // 52 
+            [50.8133, 4.3806]  // 51
+        ];
 
-        // USER POSITION
-        if (campusBounds.contains([userLat, userLng])) {
-            var marker = L.marker([userLat, userLng]).addTo(map);
-            marker.bindPopup("<b>You</b>").openPopup();
-            // PATH CREATION 
-            // create a red polyline from an array of LatLng points // Here we would have the points of the python script
-            var latlngs = [
-                [userLat, userLong],
-                [50.8125, 4.3785],
-                [50.8145, 4.3823]
-            ];
-            var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-        } else {
-            alert("You are not on campus");
-        }
+        var polyline = L.polyline(testing_latlngs, {color: 'red'}).addTo(map);
+        alert("Should have added the polyline to the map.")
+        console.log(map);
+
+
+        // // USER POSITION
+        // if (campusBounds.contains([userLat, userLng])) {
+        //     var marker = L.marker([userLat, userLng]).addTo(map);
+        //     marker.bindPopup("<b>You</b>").openPopup();
+        
+        //     // PATH CREATION 
+        //     // create a red polyline from an array of LatLng points // Here we would have the points of the python script
+        //     // var latlngs = [
+        //     //     [userLat, userLong],
+        //     //     [50.8125, 4.3785],
+        //     //     [50.8145, 4.3823]
+        //     // ];
+        //     var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+        // } else {
+        //     alert("You are not on campus");
+        // }
     });
 } else {
     alert("Geolocation is not supported by this browser.");
 }
+
+
+// Code function that suppose we receive a vector of coordinates and trace a path for it.
+/**
+ * Create a vector of coordinates
+ * Iterate over the vector and trace lines between the points
+ * enjoy
+ */
---
+++ webpage/src/welcome.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".clickable-item");
+
+    items.forEach(function (item) {
+        item.addEventListener("mouseover", function () {
+        this.style.transform = "scale(1.1)";
+        });
+
+        item.addEventListener("mouseout", function () {
+        this.style.transform = "scale(1)";
+        });
+    });
+});
+
+// Not sure if the code above is even necesarry
+// The code below is for the form toggling
+var currentFormId = null;
+
+function toggleForm(formId) {
+    var buildingForm = document.getElementById("buildingForm");
+    var classroomForm = document.getElementById("classroomForm");
+
+    // Means that the same button is clicked again
+    if (currentFormId === formId) {
+        if (formId === "buildingForm") {
+            buildingForm.classList.toggle("hidden");
+        } else if (formId === "classroomForm") {
+            classroomForm.classList.toggle("hidden");
+        }
+        // If the same button is clicked again, toggle visibility
+
+        // Means that a different button is clicked
+    } else {
+        // Hide the form associated with the previously clicked button
+        if (currentFormId === "buildingForm") {
+            buildingForm.classList.add("hidden");
+        } else if (currentFormId === "classroomForm") {
+            classroomForm.classList.add("hidden");
+        }
+
+        // Show the form associated with the currently clicked button
+        if (formId === "buildingForm") {
+            buildingForm.classList.remove("hidden");
+        } else if (formId === "classroomForm") {
+            classroomForm.classList.remove("hidden");
+        }
+
+        // Update the currentFormId
+        currentFormId = formId;
+    }
+}
+// The code below is for the form submission when clicking on the submit button
+function submitInput(formId) {
+    if (formId === "building") {
+        // Should also store user actual position from localisation.js somehow
+        var input = document.getElementById("building").value;
+        alert(input);
+    } else if (formId === "class") {
+        var inputSrc = document.getElementById("classSrc").value;
+        var inputDst = document.getElementById("classDst").value;
+        alert(inputSrc + " " + inputDst);
+    }
+    // var input = document.getElementById(formId).value;
+    // alert(input);
+}
