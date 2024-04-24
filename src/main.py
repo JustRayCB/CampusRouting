d7b3d396819e1eff1932294e1031f8b05f6f67cf
JustRayCB <rayancbpro@gmail.com>
2024-04-24 19:33:00 +0200

    Remove useless api functions

--- src/main.py
+++ src/main.py
@@ -30,16 +30,6 @@ graphs = {
 outside_graph = OutsideGraph(f"{OUTSIDE_DATA_DIR}solbosch_map_updated.json")
 
 
-class PathRequestInside(BaseModel):
-    start: str = None
-    arrival: str = None
-
-
-class PathRequestOutside(BaseModel):
-    start: tuple = None
-    arrival: str = None
-
-
 class PathRequest(BaseModel):
     start: tuple = (-1, -1)  # Coordinates
     arrival: str = ""  # Room name
@@ -189,40 +179,4 @@ def ask(request: PathRequest) -> dict:
         "path": analyse_out.analyse(),
         "instructions": analyse_in.get_instructions(),
         "images": analyse_in.get_images(),
-    }
-
-
-@app.post("/api/ask_inside")
-def read_item(request: PathRequestInside, re: Request):
-    print("L'utilisateur veut aller de", request.start, "à", request.arrival)
-    start = request.start
-    arrival = request.arrival
-    # graph = graphs[start.split("_")[0]]
-    graph = graphs["P1"]
-    if start not in graph.nodes() or arrival not in graph.nodes():
-        return Response(status_code=404)
-    d = Dijkstra(graph, start, arrival)
-    a = BAnalysePath(graph, d.path)
-    a.analyse()
-    return {
-        "path": d.path,
-        "instructions": a.get_instructions(),
-        "images": a.get_images(),
-    }
-
-
-@app.post("/api/ask_outside")
-def get_path_for_outside(request: PathRequestOutside):
-    print("L'utilisateur veut aller de", request.start, "à", request.arrival)
-    start = request.start
-    arrival = request.arrival
-    if start is None or arrival is None:
-        return Response(status_code=404)
-    n = outside_graph.find_closest_node(start)
-    d = Dijkstra(outside_graph, n, arrival)
-    print("The path is : ", d.path)
-    coordinates = [outside_graph.nodes[node]["position"] for node in d.path]
-    return {
-        "path": d.path,
-        "coordinates": coordinates,
     }
